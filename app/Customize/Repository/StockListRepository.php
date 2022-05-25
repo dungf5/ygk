c14a1fc9b209ccacc4a042b76f68314b9276d406
Pony <laphong@monotos.biz>
2022-05-25 14:42:40 +0700

    Add func getData of stock

--- app/Customize/Repository/StockListRepository.php
+++ app/Customize/Repository/StockListRepository.php
@@ -4,6 +4,7 @@ namespace Customize\Repository;
 
 
 use Customize\Entity\MstShipping;
+use Doctrine\Common\Collections\ArrayCollection;
 use Eccube\Repository\AbstractRepository;
 use Customize\Entity\StockList;
 use Symfony\Bridge\Doctrine\RegistryInterface;
@@ -20,4 +21,19 @@ class StockListRepository extends AbstractRepository
         parent::__construct($registry, StockList::class);
     }
 
+    /***
+     * @param string $product_code
+     * @param string $customer_code
+     * @return ArrayCollection|null
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function getData($product_code = '', $customer_code ='')
+    {
+        $qb = $this->createQueryBuilder('s');
+        $qb->where('s.product_code = :product_code AND s.customer_code = :customer_code')
+            ->setParameter('product_code', $product_code)
+            ->setParameter('customer_code', $customer_code);
+        return $qb
+            ->getQuery()->getOneOrNullResult();
+    }
 }
