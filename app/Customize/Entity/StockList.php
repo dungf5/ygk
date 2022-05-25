83c546eb43cd7d8ea12a7e7bb65bf0e65f325e7b
Trong <nvtrong@monotos.biz>
2022-05-25 14:35:15 +0700

    Gen Entity

--- app/Customize/Entity/StockList.php
+++ app/Customize/Entity/StockList.php
@@ -1,16 +1,4 @@
 <?php
-
-/*
- * This file is part of EC-CUBE
- *
- * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
- *
- * http://www.ec-cube.co.jp/
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Customize\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -40,11 +28,11 @@ if (!class_exists('\Customize\Entity\StockList', false)) {
          */
         private $customer_code;
         /**
-         * @ORM\Column(type="integer",nullable=false, options={"comment":"トータル在庫" ,"default":0 })
+         * @ORM\Column(type="integer",nullable=false, options={"comment":"トータル在庫"})
          */
         private $stock_num;
         /**
-         * @ORM\Column(type="integer",nullable=false, options={"comment":"引当在庫" ,"default":0 })
+         * @ORM\Column(type="integer",nullable=false, options={"comment":"引当在庫"})
          */
         private $reserve_num;
         /**
@@ -59,5 +47,101 @@ if (!class_exists('\Customize\Entity\StockList', false)) {
          * @ORM\Column(name="update_date", type="datetimetz", columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'データ更新日時'")
          */
         private $update_date;
+
+        /**
+         * @return string
+         */
+        public function getProductCode(): string
+        {
+            return $this->product_code;
+        }
+
+        /**
+         * @param string $product_code
+         */
+        public function setProductCode(string $product_code): void
+        {
+            $this->product_code = $product_code;
+        }
+
+        /**
+         * @return string
+         */
+        public function getCustomerCode(): string
+        {
+            return $this->customer_code;
+        }
+
+        /**
+         * @param string $customer_code
+         */
+        public function setCustomerCode(string $customer_code): void
+        {
+            $this->customer_code = $customer_code;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getStockNum()
+        {
+            return $this->stock_num;
+        }
+
+        /**
+         * @param mixed $stock_num
+         */
+        public function setStockNum($stock_num): void
+        {
+            $this->stock_num = $stock_num;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getReserveNum()
+        {
+            return $this->reserve_num;
+        }
+
+        /**
+         * @param mixed $reserve_num
+         */
+        public function setReserveNum($reserve_num): void
+        {
+            $this->reserve_num = $reserve_num;
+        }
+
+        /**
+         * @return \DateTime
+         */
+        public function getCreateDate(): \DateTime
+        {
+            return $this->create_date;
+        }
+
+        /**
+         * @param \DateTime $create_date
+         */
+        public function setCreateDate(\DateTime $create_date): void
+        {
+            $this->create_date = $create_date;
+        }
+
+        /**
+         * @return \DateTime
+         */
+        public function getUpdateDate(): \DateTime
+        {
+            return $this->update_date;
+        }
+
+        /**
+         * @param \DateTime $update_date
+         */
+        public function setUpdateDate(\DateTime $update_date): void
+        {
+            $this->update_date = $update_date;
+        }
     }
 }
