f62e9a34a20d3e938f847bf04e096d73eacefe20
PC00202\figo <dung_d@monotos.biz>
2022-05-31 15:22:38 +0900

    no message

--- app/Customize/Controller/Mypage/MypageController.php
+++ app/Customize/Controller/Mypage/MypageController.php
@@ -11,7 +11,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Customize\Controller\Mypage;
+namespace Customize\Controller\;
 
 use Customize\Entity\MstShipping;
 use Customize\Repository\OrderRepository;
