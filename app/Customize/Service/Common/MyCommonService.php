f331401a7f34ba9955339cda6605cd69cd20f5d3
Pony <laphong@monotos.biz>
2022-06-24 09:44:11 +0700

    comit source order

--- app/Customize/Service/Common/MyCommonService.php
+++ app/Customize/Service/Common/MyCommonService.php
@@ -177,7 +177,7 @@ class MyCommonService extends AbstractRepository
 
 
 
-        $sql = " 	select b.id AS ec_order_lineno,a.order_no,b.product_id,c.product_code,b.quantity	 from
+        $sql = " 	select b.id AS ec_order_lineno,a.order_no,b.product_id,c.product_code,c.jan_code,b.quantity	 from
 				dtb_order as a  join dtb_order_item b on a.id = b.order_id
 				join mst_product as c   on c.ec_product_id = b.product_id
 			WHERE order_no='$order_no'
@@ -587,6 +587,15 @@ class MyCommonService extends AbstractRepository
             $orderItem->setOtodokeCode($itemSave['otodoke_code']);
             $orderItem->setOrderPrice($itemSave['order_price']);
             $orderItem->setDemandQuantity($itemSave['demand_quantity']);
+            // No41 注文情報送信I/F start
+            //'order_date'=>'',   // ・受注日←受注日(購入日)
+            $orderItem->setDeliPlanDate($itemSave['deli_plan_date']);                                       // ・希望納期（納入予定日）←配送日指定
+            $orderItem->setItemNo($itemSave['item_no']);                                                    // ・客先品目No←JANコード
+            $orderItem->setDemandUnit($itemSave['demand_unit']);                                            // ・需要単位←商品情報の入り数が‘1’の場合、‘PC’、入り数が‘1’以外の場合、‘CS’
+            $orderItem->setDynaModelSeg2($itemSave['dyna_model_seg2']);                                     // ・ダイナ規格セグメント02←EC注文番号
+            $orderItem->setDynaModelSeg4($itemSave['dyna_model_seg4']);                                     // ・ダイナ規格セグメント04←EC注文番号
+            $orderItem->setDynaModelSeg6($itemSave['dyna_model_seg6']);                                     // ・ダイナ規格セグメント05←EC注文明細番号
+            // No41 注文情報送信I/F end
             $this->entityManager->persist($orderItem);
             $this->entityManager->flush();
         }
