32d0480df954aba810e496bf3190b4a3ad490eb5
Pascal <nvthuan@monotos.biz>
2023-03-27 17:29:48 +0700

    no message

--- app/Customize/Entity/MstCustomer.php
+++ app/Customize/Entity/MstCustomer.php
@@ -26,8 +26,6 @@ if (!class_exists('\Customize\Entity\MstCustomer', false)) {
          * @ORM\Column(name="ec_customer_id",type="integer",nullable=true, options={"comment":"" ,"default":1 })
          */
         private $ec_customer_id;
-
-
         /**
          * @var string
          *
@@ -100,13 +98,28 @@ if (!class_exists('\Customize\Entity\MstCustomer', false)) {
          * @ORM\Column(name="update_date", type="datetimetz", columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'データ更新日時'")
          */
         private $update_date;
-
         /**
          * @var int
          *
          * @ORM\Column(name="special_order_flg", type="integer", nullable=true)
          */
         private $special_order_flg;
+
+        /**
+         * @return string
+         */
+        public function getCustomerCode()
+        {
+            return $this->customer_code;
+        }
+
+        /**
+         * @param string $customer_code
+         */
+        public function setCustomerCode(string $customer_code)
+        {
+            $this->customer_code = $customer_code;
+        }
 
         /**
          * @return mixed
@@ -119,9 +132,201 @@ if (!class_exists('\Customize\Entity\MstCustomer', false)) {
         /**
          * @param mixed $ec_customer_id
          */
-        public function setEcCustomerId($ec_customer_id): void
+        public function setEcCustomerId($ec_customer_id)
         {
             $this->ec_customer_id = $ec_customer_id;
+        }
+
+        /**
+         * @return string
+         */
+        public function getCustomerName()
+        {
+            return $this->customer_name;
+        }
+
+        /**
+         * @param string $customer_name
+         */
+        public function setCustomerName(string $customer_name)
+        {
+            $this->customer_name = $customer_name;
+        }
+
+        /**
+         * @return string
+         */
+        public function getCompanyName()
+        {
+            return $this->company_name;
+        }
+
+        /**
+         * @param string $company_name
+         */
+        public function setCompanyName(string $company_name)
+        {
+            $this->company_name = $company_name;
+        }
+
+        /**
+         * @return string
+         */
+        public function getCompanyNameAbb()
+        {
+            return $this->company_name_abb;
+        }
+
+        /**
+         * @param string $company_name_abb
+         */
+        public function setCompanyNameAbb(string $company_name_abb)
+        {
+            $this->company_name_abb = $company_name_abb;
+        }
+
+        /**
+         * @return string
+         */
+        public function getDepartment()
+        {
+            return $this->department;
+        }
+
+        /**
+         * @param string $department
+         */
+        public function setDepartment(string $department)
+        {
+            $this->department = $department;
+        }
+
+        /**
+         * @return string
+         */
+        public function getPostalCode()
+        {
+            return $this->postal_code;
+        }
+
+        /**
+         * @param string $postal_code
+         */
+        public function setPostalCode(string $postal_code)
+        {
+            $this->postal_code = $postal_code;
+        }
+
+        /**
+         * @return string
+         */
+        public function getAddr01()
+        {
+            return $this->addr01;
+        }
+
+        /**
+         * @param string $addr01
+         */
+        public function setAddr01(string $addr01)
+        {
+            $this->addr01 = $addr01;
+        }
+
+        /**
+         * @return string
+         */
+        public function getAddr02()
+        {
+            return $this->addr02;
+        }
+
+        /**
+         * @param string $addr02
+         */
+        public function setAddr02(string $addr02)
+        {
+            $this->addr02 = $addr02;
+        }
+
+        /**
+         * @return string
+         */
+        public function getAddr03()
+        {
+            return $this->addr03;
+        }
+
+        /**
+         * @param string $addr03
+         */
+        public function setAddr03(string $addr03)
+        {
+            $this->addr03 = $addr03;
+        }
+
+        /**
+         * @return string
+         */
+        public function getEmail()
+        {
+            return $this->email;
+        }
+
+        /**
+         * @param string $email
+         */
+        public function setEmail(string $email)
+        {
+            $this->email = $email;
+        }
+
+        /**
+         * @return string
+         */
+        public function getPhoneNumber()
+        {
+            return $this->phone_number;
+        }
+
+        /**
+         * @param string $phone_number
+         */
+        public function setPhoneNumber(string $phone_number)
+        {
+            $this->phone_number = $phone_number;
+        }
+
+        /**
+         * @return \DateTime
+         */
+        public function getCreateDate()
+        {
+            return $this->create_date;
+        }
+
+        /**
+         * @param $create_date
+         */
+        public function setCreateDate($create_date)
+        {
+            $this->create_date = $create_date;
+        }
+
+        /**
+         * @return \DateTime
+         */
+        public function getUpdateDate()
+        {
+            return $this->update_date;
+        }
+
+        /**
+         * @param $update_date
+         */
+        public function setUpdateDate($update_date)
+        {
+            $this->update_date = $update_date;
         }
 
         /**
@@ -129,15 +334,16 @@ if (!class_exists('\Customize\Entity\MstCustomer', false)) {
          */
         public function getSpecialOrderFlg(): int
         {
-            return (int)$this->special_order_flg;
+            return $this->special_order_flg;
         }
 
         /**
          * @param int $special_order_flg
          */
-        public function setSpecialOrderFlg(int $special_order_flg): void
+        public function setSpecialOrderFlg(int $special_order_flg)
         {
             $this->special_order_flg = $special_order_flg;
         }
+
     }
 }
