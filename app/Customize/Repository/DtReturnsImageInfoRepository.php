d1382ebde0dfd2b612bfd2afb2aa97515dc39418
Max - HaiHuynh <hdhai@monotos.biz>
2023-03-31 16:32:34 +0700

    9103 : form receipt

--- app/Customize/Repository/DtReturnsImageInfoRepository.php
+++ app/Customize/Repository/DtReturnsImageInfoRepository.php
@@ -29,12 +29,42 @@ class DtReturnsImageInfoRepository extends AbstractRepository
                 $object->setReturnsNo($data['returns_no']);
             }
             
-            $object->setCusImageUrlPath1($data['cus_image_url_path1']);
-            $object->setCusImageUrlPath2($data['cus_image_url_path2']);
-            $object->setCusImageUrlPath3($data['cus_image_url_path3']);
-            $object->setCusImageUrlPath4($data['cus_image_url_path4']);
-            $object->setCusImageUrlPath5($data['cus_image_url_path5']);
-            $object->setCusImageUrlPath6($data['cus_image_url_path6']);
+            if( !empty( $data['cus_image_url_path1'] ) ) {
+                $object->setCusImageUrlPath1($data['cus_image_url_path1']);
+            }
+            if( !empty( $data['cus_image_url_path2'] ) ) {
+                $object->setCusImageUrlPath2($data['cus_image_url_path2']);
+            }
+            if( !empty( $data['cus_image_url_path3'] ) ) {
+                $object->setCusImageUrlPath3($data['cus_image_url_path3']);
+            }
+            if( !empty( $data['cus_image_url_path4'] ) ) {
+                $object->setCusImageUrlPath4($data['cus_image_url_path4']);
+            }
+            if( !empty( $data['cus_image_url_path5'] ) ) {
+                $object->setCusImageUrlPath5($data['cus_image_url_path5']);
+            }
+            if( !empty( $data['cus_image_url_path6'] ) ) {
+                $object->setCusImageUrlPath6($data['cus_image_url_path6']);
+            }
+            if( !empty( $data['stock_image_url_path1'] ) ) {
+                $object->setStockImageUrlPath1($data['stock_image_url_path1']);
+            }
+            if( !empty( $data['stock_image_url_path2'] ) ) {
+                $object->setStockImageUrlPath2($data['stock_image_url_path2']);
+            }
+            if( !empty( $data['stock_image_url_path3'] ) ) {
+                $object->setStockImageUrlPath3($data['stock_image_url_path3']);
+            }
+            if( !empty( $data['stock_image_url_path4'] ) ) {
+                $object->setStockImageUrlPath4($data['stock_image_url_path4']);
+            }
+            if( !empty( $data['stock_image_url_path5'] ) ) {
+                $object->setStockImageUrlPath5($data['stock_image_url_path5']);
+            }
+            if( !empty( $data['stock_image_url_path6'] ) ) {
+                $object->setStockImageUrlPath6($data['stock_image_url_path6']);
+            }
 
             $this->getEntityManager()->persist($object);
             $this->getEntityManager()->flush();
