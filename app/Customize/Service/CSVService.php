618da6d72c92be9e7ad65ff0a12c6acb93882f58
Pascal <nvthuan@monotos.biz>
2023-04-18 14:13:23 +0700

    Change way to get file from FTP Server

--- app/Customize/Service/CSVService.php
+++ app/Customize/Service/CSVService.php
@@ -13,9 +13,20 @@
 
 namespace Customize\Service;
 
+use Customize\Entity\DtImportCSV;
+use Doctrine\ORM\EntityManagerInterface;
+
 class CSVService
 {
     use CurlPost;
+
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 
     /***
      * @param string $path
@@ -50,6 +61,103 @@ class CSVService
             return [
                 'status' => 0,
                 'message' => "File {$path} ".$e->getMessage(),
+            ];
+        }
+    }
+
+    public function transferFile($path_from, $path_to, $file)
+    {
+        try {
+            if (empty($file)) {
+                return [
+                    'status' => 0,
+                    'message' => 'File is empty',
+                ];
+            }
+
+            $temp_path_local = '';
+
+            if (getenv('APP_IS_LOCAL') == 1) {
+                $temp_path_local = '.';
+                $path_from = '.'.$path_from;
+                $path_to = '.'.$path_to;
+            }
+
+            if (file_exists($path_from.$file) == false) {
+                $this->pushGoogleChat('path: '.$path_from.$file.' is invalid');
+
+                return [
+                    'status' => 0,
+                    'message' => 'path: '.$path_from.$file.' is invalid',
+                ];
+            }
+
+            // Create directory local if have'n
+            $arr_path_local = explode('/', $path_to);
+
+            foreach ($arr_path_local as $subDir) {
+                if (empty($subDir)) {
+                    continue;
+                }
+                $temp_path_local .= '/'.$subDir;
+
+                if (file_exists($temp_path_local) == false) {
+                    mkdir($temp_path_local);
+                }
+            }
+            $temp_path_local = null;
+            // End - Create directory local if have'n
+
+            $yearDir = $path_to.date('Y');
+            if (file_exists($yearDir) == false) {
+                mkdir($yearDir);
+            }
+
+            $monthDir = $path_to.date('Y/m');
+            if (file_exists($monthDir) == false) {
+                mkdir($monthDir);
+            }
+
+            $local_file_name = date('YmdHis').'.csv';
+            $local_file = $monthDir.'/'.$local_file_name;
+
+            // try to copy file from path_from to path_to
+            if (copy($path_from.$file, $local_file_name)) {
+                // Save file information to DB
+                $insertDate = [
+                    'file_name' => $local_file_name,
+                    'directory' => $monthDir,
+                    'message' => null,
+                    'is_sync' => 0,
+                    'is_error' => 0,
+                    'is_send_mail' => 0,
+                    'in_date' => new \DateTime(),
+                    'up_date' => null,
+                ];
+
+                $this->entityManager->getRepository(DtImportCSV::class)->insertData($insertDate);
+
+                $message = "successfully written {$file} to {$local_file}";
+                unlink($path_from.$file);
+                $this->pushGoogleChat("successfully written {$file} to {$local_file}");
+
+            } else {
+                $message = "There was a problem while downloading {$file} to {$local_file}";
+
+                $this->pushGoogleChat("There was a problem while downloading {$file} to {$local_file}");
+            }
+
+            return [
+                'status' => 1,
+                'message' => $message,
+            ];
+
+        } catch (\Exception $e) {
+            $this->pushGoogleChat($e->getMessage());
+
+            return [
+                'status' => -1,
+                'message' => $e->getMessage(),
             ];
         }
     }
