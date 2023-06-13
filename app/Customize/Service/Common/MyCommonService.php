b45d81e23ff68182b33a76770279120cc8753560
Pascal <nvthuan@monotos.biz>
2023-06-13 09:41:07 +0700

    no message

--- app/Customize/Service/Common/MyCommonService.php
+++ app/Customize/Service/Common/MyCommonService.php
@@ -90,7 +90,9 @@ class MyCommonService extends AbstractRepository
                     a.update_date,
                     a.email as customer_email,
                     a.special_order_flg,
-                    a.price_view_flg
+                    a.price_view_flg,
+                    a.fusrdec1,
+                    a.pdf_export_flg
          ';
 
         $sql = " SELECT $column   FROM mst_customer a join `dtb_customer` `dtcus` on((`dtcus`.`id` = `a`.`ec_customer_id`))  WHERE ec_customer_id=?";
