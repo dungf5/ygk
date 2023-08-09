f3c35ce999cbac811a001cd505584b1640e62a2e
nvthuan@monotos.biz <nvthuan@monotos.biz>
2023-08-09 10:10:10 +0700

    no message

--- app/Customize/Controller/Mypage/MypageController.php
+++ app/Customize/Controller/Mypage/MypageController.php
@@ -1926,6 +1926,10 @@ class MypageController extends AbstractController
      */
     public function exportPdfMultiple(Request $request)
     {
+        set_time_limit(0);
+        ini_set('memory_limit', '-1');
+        ini_set('max_execution_time', 0);
+
         $htmlFileName = 'Mypage/exportPdfMultiple.twig';
         $preview = MyCommon::getPara('preview');
         $delivery_no = MyCommon::getPara('delivery_no');
