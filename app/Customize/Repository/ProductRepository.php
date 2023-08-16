67445dd1d9289c372c50d2667c45e0f3373bec60
nvthuan@monotos.biz <nvthuan@monotos.biz>
2023-08-16 15:14:00 +0700

    no message

--- app/Customize/Repository/ProductRepository.php
+++ app/Customize/Repository/ProductRepository.php
@@ -1,22 +1,31 @@
 <?php
 
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Customize\Repository;
 
-
-use Customize\Entity\MstShipping;
 use Customize\Service\Common\MyCommonService;
 use Customize\Service\GlobalService;
 use Doctrine\ORM\Query\Expr\Join;
 use Eccube\Common\EccubeConfig;
 use Eccube\Doctrine\Query\Queries;
-use Eccube\Repository\AbstractRepository;
 use Eccube\Entity\Product;
+use Eccube\Repository\AbstractRepository;
 use Eccube\Repository\QueryKey;
 use Eccube\Util\StringUtil;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+
 class ProductRepository extends AbstractRepository
 {
-
     /**
      * @var Queries
      */
@@ -33,17 +42,12 @@ class ProductRepository extends AbstractRepository
     protected $globalService;
 
     public const COLUMNS = [
-        'product_id' => 'p.id'
-        ,'name' => 'p.name'
-        ,'product_code' => 'pc.code'
-        ,'stock' => 'pc.stock'
-        ,'status' => 'p.Status'
-        ,'create_date' => 'p.create_date'
-        ,'update_date' => 'p.update_date'
+        'product_id' => 'p.id', 'name' => 'p.name', 'product_code' => 'pc.code', 'stock' => 'pc.stock', 'status' => 'p.Status', 'create_date' => 'p.create_date', 'update_date' => 'p.update_date',
     ];
 
     /**
      * ProductRepository constructor.
+     *
      * @param RegistryInterface $registry
      * @param Queries $queries
      * @param EccubeConfig $eccubeConfig
@@ -53,8 +57,7 @@ class ProductRepository extends AbstractRepository
         Queries $queries,
         EccubeConfig $eccubeConfig,
         GlobalService $globalService
-    )
-    {
+    ) {
         parent::__construct($registry, Product::class);
         $this->queries = $queries;
         $this->eccubeConfig = $eccubeConfig;
@@ -114,7 +117,7 @@ class ProductRepository extends AbstractRepository
             $qb->groupBy('p.id');
             $qb->orderBy('price02_min', 'ASC');
             $qb->addOrderBy('p.id', 'DESC');
-            // 価格高い順
+        // 価格高い順
         } elseif (!empty($searchData['orderby']) && $searchData['orderby']->getId() == $config['eccube_product_order_price_higher']) {
             $qb->addSelect('MAX(pc.price02) as HIDDEN price02_max');
             $qb->innerJoin('p.ProductClasses', 'pc');
@@ -122,7 +125,7 @@ class ProductRepository extends AbstractRepository
             $qb->groupBy('p.id');
             $qb->orderBy('price02_max', 'DESC');
             $qb->addOrderBy('p.id', 'DESC');
-            // 新着順
+        // 新着順
         } elseif (!empty($searchData['orderby']) && $searchData['orderby']->getId() == $config['eccube_product_order_newer']) {
             // 在庫切れ商品非表示の設定が有効時対応
             // @see https://github.com/EC-CUBE/ec-cube/issues/1998
@@ -141,20 +144,19 @@ class ProductRepository extends AbstractRepository
             $qb
                 ->addOrderBy('p.id', 'DESC');
         }
-        if(!$user) {
+        if (!$user) {
             $customer_code = '';
         }
 
-        $qb->innerJoin('Customize\Entity\MstProduct', 'mstProduct',Join::WITH,'mstProduct.ec_product_id = p.id');
+        $qb->innerJoin('Customize\Entity\MstProduct', 'mstProduct', Join::WITH, 'mstProduct.ec_product_id = p.id');
 
-        $qb->leftJoin('Customize\Entity\Price', 'price',Join::WITH,'price.product_code = mstProduct.product_code AND price.customer_code = :customer_code')
+        $qb->leftJoin('Customize\Entity\Price', 'price', Join::WITH, 'price.product_code = mstProduct.product_code AND price.customer_code = :customer_code')
             ->setParameter(':customer_code', $customer_code);
 
-        $listSelectMstProduct = "mstProduct.product_code,mstProduct.unit_price as mst_unit_price ,mstProduct.product_name";
-        $listSelectMstProduct.=",mstProduct.quantity as mst_quantity,mstProduct.jan_code ";
+        $listSelectMstProduct = 'mstProduct.product_code,mstProduct.unit_price as mst_unit_price ,mstProduct.product_name';
+        $listSelectMstProduct .= ',mstProduct.quantity as mst_quantity,mstProduct.jan_code ';
         $qb->addSelect($listSelectMstProduct);
         $qb->addSelect('price.price_s01 as  price_s01');
-
 
         return $this->queries->customize(QueryKey::PRODUCT_SEARCH, $qb, $searchData);
     }
@@ -177,8 +179,6 @@ class ProductRepository extends AbstractRepository
         $qb->addSelect($defaultSortLoginorderPrice);
 
         // category
-        $categoryJoin = false;
-
         if (!empty($searchData['category_id']) && $searchData['category_id']) {
             $Categories = $searchData['category_id'];
 
@@ -187,14 +187,7 @@ class ProductRepository extends AbstractRepository
                     ->innerJoin('pct.Category', 'c')
                     ->andWhere($qb->expr()->in('pct.Category', ':Categories'))
                     ->setParameter('Categories', $Categories);
-
-                $categoryJoin = true;
             }
-        } else {
-            $qb->innerJoin('p.ProductCategories', 'pct')
-                ->innerJoin('pct.Category', 'c');
-
-            $categoryJoin = true;
         }
 
         $newComs = new MyCommonService($this->getEntityManager());
@@ -300,12 +293,6 @@ class ProductRepository extends AbstractRepository
             $qb->orderBy('p.create_date', 'DESC');
             $qb->addOrderBy('p.id', 'DESC');
         } else {
-            if ($categoryJoin === false) {
-                $qb
-                    ->leftJoin('p.ProductCategories', 'pct')
-                    ->leftJoin('pct.Category', 'c');
-            }
-
             // 新着順 orderby=0
             if (!$user) {
                 $qb->addOrderBy('mstProduct.unit_price', 'asc');
